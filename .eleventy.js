08fa843fa079160ddf63ccfb3f97bdc4b2161177
Robert C. Bowman III <bowman@opuscule.com>
2021-11-28 14:07:55 -0500

    Template-tized

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+	
+	eleventyConfig.addPassthroughCopy('./src/css/styles.css');
+	eleventyConfig.addPassthroughCopy('./src/images');
+	eleventyConfig.addPassthroughCopy('./src/_redirects');
+	return {
+		dir: {
+			input:"src",
+			output:"public"
+		},
+		passthroughFileCopy: true
+	};	
+}
